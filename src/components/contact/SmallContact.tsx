c30931bbc49b062404c286c5bf3445ea60864fd0
Timothy Altemus <tim@tastypool.com>
2022-02-06 00:31:13 -0600

    fix merge conflicts

--- src/components/contact/SmallContact.tsx
+++ src/components/contact/SmallContact.tsx
@@ -7,24 +7,14 @@ const SmallContact = () => {
         <div className="row">
           <div
             className="col-md-6 animate-box"
-<<<<<<< HEAD
             data-animate-effect="fadeInLeft">
-=======
-            data-animate-effect="fadeInLeft"
-          >
->>>>>>> main
             <h2>Get in Touch!</h2>
           </div>
         </div>
         <div className="row">
           <div
             className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
-<<<<<<< HEAD
             data-animate-effect="fadeInLeft">
-=======
-            data-animate-effect="fadeInLeft"
-          >
->>>>>>> main
             <p className="colorlib-lead">
               Separated they live in Bookmarksgrove right at the coast of the
               Semantics, a large language ocean.
