bcc1f43ba68b3766699bf164c4dfc6d826438892
Timothy Altemus <tim@tastypool.com>
2022-02-08 23:59:22 -0600

    feat: Add cloud function to send email

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,44 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import { transportEmail } from './email/transport_email';
+import { Status } from './models/status';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const sendEmail = functions.https.onRequest(async (req, res) => {
+  const { email, message, name, phoneNumber } = req.body;
+
+  const errors: string[] = [];
+  if (!email) {
+    errors.push('Email is required.');
+  }
+
+  if (!message) {
+    errors.push('Message is required.');
+  }
+
+  if (!name) {
+    errors.push('Name is required.');
+  }
+
+  if (!phoneNumber) {
+    errors.push('Phone number is required.');
+  }
+
+  if (errors.length > 0) {
+    res.status(400).json({ errors, status: Status.BAD_REQUEST });
+    return;
+  }
+
+  const body: string =
+    message + `\n\n- ${name}\np: ${phoneNumber}\ne: ${email}`;
+  const subject = `New message from ${name} - ${email}`;
+
+  try {
+    const details = await transportEmail(email, subject, body);
+    res.status(200).json({ details, status: Status.SUCCESS });
+  } catch (error) {
+    functions.logger.error(error);
+    res.status(500).json({ error, status: Status.ERROR });
+  }
+});
