4ecb67c7425f13807b6be9c8667cae3b10ddbbdb
timothyaltemus <76897954+timothyaltemus@users.noreply.github.com>
2022-02-10 15:27:05 -0600

    fix: add environment variable to production build of all (#47)
    Co-authored-by: Timothy Altemus <tim@tastypool.com>

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,9 +1,7 @@
 import { Config } from './models/config';
 
-if (!process.env.REACT_APP_FIREBASE_URL) {
-  throw Error('REACT_APP_FIREBASE_URL is not defined');
-}
-
 export const config: Config = {
-  firebaseUrl: process.env.REACT_APP_FIREBASE_URL,
+  firebaseUrl:
+    process.env.REACT_APP_FIREBASE_URL ??
+    'http://localhost:5001/arumi-design/us-central1/sendEmail',
 };
