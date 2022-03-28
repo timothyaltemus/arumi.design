542be901ff696b52cbdb019c8c8f19da6bd84b1a
Timothy Altemus <tim@tastypool.com>
2022-03-27 23:53:17 -0500

    feat: reorder work

--- app/src/data/works.ts
+++ app/src/data/works.ts
@@ -4,43 +4,6 @@ import { getRandomNumber } from '../utils/random';
 // TODO: Migrate all of this to a database (e.g. Firebase Storage)
 export const works: Work[] = [
   {
-    id: '2a9c7328-8f22-40ca-84af-be0dd034ced1',
-    title:
-      'User Persona - Brazilian Steakhouse Food Delivery App (Google UX Course)',
-    description: [
-      'As part of my Google UX project, I created two User Personas to reflect a group of users pain points.',
-      "Each Persona I created represents a group of users with similar characteristics that I learned about through my research. Each Persona is unique because they reflect the lifestyles of the users and gives me an idea of how to meet users' needs or challenges.",
-      'As I was creating Personas, I looked for the most common themes in my data and grouped the users who personify those themes together.',
-      'I used the information in Personas to create designs that speak directly to users, like Katerina and Michael. This ensures users have the best experience when using my product!',
-    ],
-    thumbnail: 'thumbnail-1.png',
-    images: ['work-1.png'],
-    platform: 'Mobile',
-    tags: ['User Persona', 'UX', 'Google UX Course', 'Mobile'],
-    role: 'UX Researcher',
-    timeline: '1 Day',
-    type: 'User Persona',
-    views: getRandomNumber({ max: 304, min: 178 }),
-  },
-  {
-    id: 'd0e86a09-d0d0-478e-86a8-4b717d802334',
-    title:
-      'User Journey Map - Brazilian Steakhouse Food Delivery App (Google UX Course)',
-    description: [
-      'For my Google UX project, I created two User Journey Maps built off the personas I had previously created when conducting my research.',
-      "Creating a User Journey Map let me thoroughly document the entire sequence of events and interactions a user experiences, including the user's interaction with my design. This way I could really focus on how a specific persona thinks and feels at every step of the journey. User journey mapping also highlighted some new pain points I took into consdierations for my designs.",
-      "Through the User Journey Map, I identified some of the user's emotions, which allowed me to improve the overall user experience for all users.",
-    ],
-    thumbnail: 'thumbnail-2.png',
-    images: ['work-2.png'],
-    views: getRandomNumber({ max: 304, min: 178 }),
-    tags: ['User Journey', 'UX', 'Google UX Course', 'Mobile'],
-    role: 'UX Researcher',
-    timeline: '5 Days',
-    platform: 'Mobile',
-    type: 'User Journey Map',
-  },
-  {
     id: '4ca850dc-636c-452a-80bc-e1f1020fde08',
     title: "Baker Hughes's DS Store Website Heuristic Evaluations",
     description: [
@@ -58,45 +21,6 @@ export const works: Work[] = [
     timeline: '3 Weeks',
     platform: 'Desktop',
     type: 'Heurisitc Evaluations',
-  },
-  {
-    id: '2f58950c-4854-4eed-89d5-012a2fd07d56',
-    title:
-      'Building Wireframes - Brazilian Steakhouse Food Delivery App (Google UX Course)',
-    description: [
-      'For my Google UX Project, I am in the process of designing a mobile food delivery app for a fictionial restaraunt in the Texas area.',
-      'Before creating a functioning prototype, I started by sketching out and eventually turning my designs into  lo-fi Wireframes using Figma. Each Wireframe I created ensures that I solved a real problem users typically faced while working with food delivery apps on their phones. I chose to stick with a lower amount of complexity as I was inspired by my research and wanted to get ideas designed quickly.',
-      'While designing my Wireframes, I incorporated my knowledge of Gestalt Principles. Gestalt Principles describes how humans group similar elements, recognize patterns, and simplify complex images when we perceive objects.',
-      'For the first principle, Similarity, I wanted to keep "like with like" for consistency. For example, in the Rewards page, I made sure that the placeholders for icons are the same shape and size in all my  wireframes.',
-      'Next, by using Proximity, I ensured that I placed buttons such as "Place Order", "Sign In" and "Regsiter", realtively close to the content to make a connection between the two elements.',
-      'For the last principle, Common Region, I created a border around the navigation bar. This design tip helps users recognize that the icons in the navigation bar are more important than others and are perceived to be grouped together.',
-      'I used simple rectangles, shapes and icons to get my word across in my designs. My main focus while creating these Wireframes were functionality. After completing these Wireframes, I went on to create prototypes in Figma and conducted a Usability Study for my Wireframes.',
-    ],
-    thumbnail: 'thumbnail-4.png',
-    images: ['work-4.png'],
-    views: getRandomNumber({ max: 304, min: 178 }),
-    tags: ['Wireframes', 'Prototype', 'UX', 'Google UX Course', 'Mobile'],
-    role: 'UX Designer',
-    timeline: '3 days',
-    platform: 'Mobile',
-    type: 'Wireframes & Prototype',
-  },
-  {
-    id: 'b849d85e-cb9c-41cd-953c-55fbb40c72fc',
-    title: 'Delish - Recipe App',
-    description: [
-      'Figma is a browser based, powerful design tool. I sparked an interest in learning Figma after seeing designers on my team carefully curate designs based on the research that was provided to them.',
-      'I took an online course on Figma where I participated in a project called "Chef\'s Recipe App". Through this course, I was able to discover the Figma interface and learned to use powerful tools like constraints to make my designs responsive, and components to build a library of design elements to use throughout my future projects.',
-      'Through this training, I was able to learn the essentials of Figma, how to design mobile and desktop apps as well as wireframing.',
-    ],
-    thumbnail: 'thumbnail-5.png',
-    images: ['work-5.png'],
-    views: getRandomNumber({ max: 304, min: 178 }),
-    tags: ['Design', 'Prototype', 'UX', 'Mobile'],
-    role: 'UX Designer',
-    timeline: '1 Week',
-    platform: 'Mobile',
-    type: 'Design & Prototype',
   },
   {
     id: 'e19537bc-f95a-40cc-83a0-cd221a28a5c9',
@@ -153,7 +77,7 @@ export const works: Work[] = [
     thumbnail: 'thumbnail-8.png',
     description: [
       'For my Google UX Course project, I created a highly effective Research Study Plan. There are seven elements that I included: the project background, the research goals, the detailed research questions, the key performance indicators or KPIs, the methodology, the participants, and the script or questions I asked the participants.',
-      'The first element of my plan is the project background. The project background answers the question: "What led me to conduct this research"? I created a mobile application for a local restaurant that allows its users to place orders online through this application.  This led me to determine "What design problems am I trying to solve? And how will the results of the research impact my design decisions?". I used these answers to create goals for my research project.',
+      'The first element of my plan is the project background. The project background answers the question: "What led me to conduct this research"? I created a mobile application for a local restaurant that allows its users to place orders online through this application.  This led me to determine "What design problems am I trying to solve? And how will the results of the research impact my design decisions"? I used these answers to create goals for my research project.',
       'The goals for the research project helped me understand key problems such as determining whether this application was difficult to use. I needed to consider the ease of use in order to understand why customers join this app and leave or stay. After I determined my research goals, I developed detailed research questions for my plan such as "how long does it take a user to find and order an item from the menu of the app? And what can I learn from the user flow or the steps that users take to place an order"?',
       'I measured my progress using KPI’s such as time on task and conversion rates. For my app, one thing I wanted to track is how many users in the research study complete their search for a menu item and how long they spend on this task. The next element of my plan is the methodology. This is where I documented the steps I took to conduct my research. I needed to determine "How will I collect data and how will I analyze the data once I get it"? To find out if users are able to find and order in the new app, I conducted a survey of prospective users. I recruited frequent customers of this restaurant as well as of competing restaurants in the area to participate in the survey and help determine next steps in my research process.',
     ],
@@ -171,7 +95,83 @@ export const works: Work[] = [
       },
     ],
   },
-].reverse() as Work[];
+  {
+    id: '2a9c7328-8f22-40ca-84af-be0dd034ced1',
+    title:
+      'User Persona - Brazilian Steakhouse Food Delivery App (Google UX Course)',
+    description: [
+      'As part of my Google UX project, I created two User Personas to reflect a group of users pain points.',
+      "Each Persona I created represents a group of users with similar characteristics that I learned about through my research. Each Persona is unique because they reflect the lifestyles of the users and gives me an idea of how to meet users' needs or challenges.",
+      'As I was creating Personas, I looked for the most common themes in my data and grouped the users who personify those themes together.',
+      'I used the information in Personas to create designs that speak directly to users, like Katerina and Michael. This ensures users have the best experience when using my product!',
+    ],
+    thumbnail: 'thumbnail-1.png',
+    images: ['work-1.png'],
+    platform: 'Mobile',
+    tags: ['User Persona', 'UX', 'Google UX Course', 'Mobile'],
+    role: 'UX Researcher',
+    timeline: '1 Day',
+    type: 'User Persona',
+    views: getRandomNumber({ max: 304, min: 178 }),
+  },
+  {
+    id: 'd0e86a09-d0d0-478e-86a8-4b717d802334',
+    title:
+      'User Journey Map - Brazilian Steakhouse Food Delivery App (Google UX Course)',
+    description: [
+      'For my Google UX project, I created two User Journey Maps built off the personas I had previously created when conducting my research.',
+      "Creating a User Journey Map let me thoroughly document the entire sequence of events and interactions a user experiences, including the user's interaction with my design. This way I could really focus on how a specific persona thinks and feels at every step of the journey. User journey mapping also highlighted some new pain points I took into consdierations for my designs.",
+      "Through the User Journey Map, I identified some of the user's emotions, which allowed me to improve the overall user experience for all users.",
+    ],
+    thumbnail: 'thumbnail-2.png',
+    images: ['work-2.png'],
+    views: getRandomNumber({ max: 304, min: 178 }),
+    tags: ['User Journey', 'UX', 'Google UX Course', 'Mobile'],
+    role: 'UX Researcher',
+    timeline: '5 Days',
+    platform: 'Mobile',
+    type: 'User Journey Map',
+  },
+  {
+    id: '2f58950c-4854-4eed-89d5-012a2fd07d56',
+    title:
+      'Building Wireframes - Brazilian Steakhouse Food Delivery App (Google UX Course)',
+    description: [
+      'For my Google UX Project, I am in the process of designing a mobile food delivery app for a fictionial restaraunt in the Texas area.',
+      'Before creating a functioning prototype, I started by sketching out and eventually turning my designs into  lo-fi Wireframes using Figma. Each Wireframe I created ensures that I solved a real problem users typically faced while working with food delivery apps on their phones. I chose to stick with a lower amount of complexity as I was inspired by my research and wanted to get ideas designed quickly.',
+      'While designing my Wireframes, I incorporated my knowledge of Gestalt Principles. Gestalt Principles describes how humans group similar elements, recognize patterns, and simplify complex images when we perceive objects.',
+      'For the first principle, Similarity, I wanted to keep "like with like" for consistency. For example, in the Rewards page, I made sure that the placeholders for icons are the same shape and size in all my  wireframes.',
+      'Next, by using Proximity, I ensured that I placed buttons such as "Place Order", "Sign In" and "Regsiter", realtively close to the content to make a connection between the two elements.',
+      'For the last principle, Common Region, I created a border around the navigation bar. This design tip helps users recognize that the icons in the navigation bar are more important than others and are perceived to be grouped together.',
+      'I used simple rectangles, shapes and icons to get my word across in my designs. My main focus while creating these Wireframes were functionality. After completing these Wireframes, I went on to create prototypes in Figma and conducted a Usability Study for my Wireframes.',
+    ],
+    thumbnail: 'thumbnail-4.png',
+    images: ['work-4.png'],
+    views: getRandomNumber({ max: 304, min: 178 }),
+    tags: ['Wireframes', 'Prototype', 'UX', 'Google UX Course', 'Mobile'],
+    role: 'UX Designer',
+    timeline: '3 days',
+    platform: 'Mobile',
+    type: 'Wireframes & Prototype',
+  },
+  {
+    id: 'b849d85e-cb9c-41cd-953c-55fbb40c72fc',
+    title: 'Delish - Recipe App',
+    description: [
+      'Figma is a browser based, powerful design tool. I sparked an interest in learning Figma after seeing designers on my team carefully curate designs based on the research that was provided to them.',
+      'I took an online course on Figma where I participated in a project called "Chef\'s Recipe App". Through this course, I was able to discover the Figma interface and learned to use powerful tools like constraints to make my designs responsive, and components to build a library of design elements to use throughout my future projects.',
+      'Through this training, I was able to learn the essentials of Figma, how to design mobile and desktop apps as well as wireframing.',
+    ],
+    thumbnail: 'thumbnail-5.png',
+    images: ['work-5.png'],
+    views: getRandomNumber({ max: 304, min: 178 }),
+    tags: ['Design', 'Prototype', 'UX', 'Mobile'],
+    role: 'UX Designer',
+    timeline: '1 Week',
+    platform: 'Mobile',
+    type: 'Design & Prototype',
+  },
+];
 
 export const getProject = (id: string): [Work, number, number] => {
   const index = works.findIndex((project) => project.id === id);
